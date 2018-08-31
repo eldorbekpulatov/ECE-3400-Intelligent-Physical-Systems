cb1e199a612be553558743aedfc485a5dbd90cd6
Eldor Bekpulatov <eb654@cornell.edu>
2018-08-31 16:10:12 -0400

    Initial Heroku commit

--- website/settings.py
+++ website/settings.py
@@ -20,13 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hbi@91&$)j#-lg-6=@3f+=#!!97)-#_q-3*+w3#pnqf=di1ch1'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'hbi@91&$)j#-lg-6=@3f+=#!!97)-#_q-3*+w3#pnqf=di1ch1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS =  ['ece3400.herokuapp.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Application definition
 
