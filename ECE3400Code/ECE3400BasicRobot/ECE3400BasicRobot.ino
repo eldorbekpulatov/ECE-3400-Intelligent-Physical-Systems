1f6b0c19bb372abbcede7f17151bf8190c713e24
jp2228 <jp2228@cornell.edu>
2018-08-31 19:29:43 -0400

    ECE 3400 Lab 1 code

---
+++ ECE3400Code/ECE3400BasicRobot/ECE3400BasicRobot.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+// the setup function runs once when you press reset or power the board
+Servo rightWheel;
+Servo leftWheel;
+int input = A0;
+
+void setup() {
+  Serial.begin(9600);
+  rightWheel.attach(11);
+  leftWheel.attach(10);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  dance();
+}
+
+void dance(){
+  rightWheel.write(0);
+  leftWheel.write(0);
+  delay(1000);
+  leftWheel.write(180);
+  delay(1000);
+  leftWheel.write(0);
+  rightWheel.write(180);
+  delay(1000);
+  leftWheel.write(180);
+  rightWheel.write(180);
+  delay(1000);
+}
+
