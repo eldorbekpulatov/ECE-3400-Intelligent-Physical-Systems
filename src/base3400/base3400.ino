9ca2616f092e986a470a62f3fc067ca55cebcc4c
JThr3e <j768miner@gmail.com>
2018-11-30 01:53:16 -0500

    fixing radio bugs pt1

--- src/base3400/base3400.ino
+++ src/base3400/base3400.ino
@@ -1,62 +1,86 @@
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include "nRF24L01.h"
+#include "RF24.h"
 // 
 RF24 radio(9, 10); // CE, CSN
-long long address = 0x0000000068LL;
+
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pipes[2] = { 0x0000000044LL, 0x0000000045LL };
+
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // Wait for serial port to connect. Needed for native USB port only
-  }
   setupRadio();
 }
 
 void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    String guiMsg = "";
-    guiMsg += String(int(text[0]));
-    guiMsg += ","+String(int(text[1]));
-    byte mazeInfo = text[2];
-    if(wallNorth(mazeInfo)){
-      guiMsg+=",north=true";
-    } else {
-      guiMsg+=",north=false";
-    }
-    if(wallEast(mazeInfo)){
-      guiMsg+=",east=true";
-    } else {
-      guiMsg+=",east=false";
-    }
-    if(wallWest(mazeInfo)){
-      guiMsg+=",west=true";
-    } else {
-      guiMsg+=",west=false";
-    }
-    if(wallSouth(mazeInfo)){
-      guiMsg+=",south=true";
-    } else {
-      guiMsg+=",south=false";
-    }
+    if ( radio.available() )
+    {
+      
+      bool done = false;
+      while (!done)
+      {
+        // Fetch the payload, and see if this was the last one.
+        //done = radio.read( &got_time, sizeof(unsigned long) );
+        char text[32] = "";
+        done = radio.read(&text, sizeof(text));
+        String guiMsg = "";
+        guiMsg += String(int(text[0]));
+        guiMsg += ","+String(int(text[1]));
+        byte mazeInfo = text[2];
+        if(wallNorth(mazeInfo)){
+          guiMsg+=",north=true";
+        } else {
+          guiMsg+=",north=false";
+        }
+        if(wallEast(mazeInfo)){
+          guiMsg+=",east=true";
+        } else {
+          guiMsg+=",east=false";
+        }
+        if(wallWest(mazeInfo)){
+          guiMsg+=",west=true";
+        } else {
+          guiMsg+=",west=false";
+        }
+        if(wallSouth(mazeInfo)){
+          guiMsg+=",south=true";
+        } else {
+          guiMsg+=",south=false";
+        }
 
     if(tShape(mazeInfo)!="none"){
       guiMsg+=",tshape=" +(tShape(mazeInfo));
       guiMsg+=",tcolor=" +(tColor(mazeInfo));
-      Serial.println(guiMsg);
     }
-  }
+    Serial.println(guiMsg);
+    delay(20);
+
+    }
+ }
 }
 
 /**** SET UP ****/
 
 void setupRadio(){
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+  // optionally, increase the delay between retries & # of retries
+  radio.setRetries(15,15);
+  radio.setAutoAck(true);
+  // set the channel
+  radio.setChannel(0x50);
+  // set the power
+  // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_MED=-6dBM, and RF24_PA_HIGH=0dBm.
+  radio.setPALevel(RF24_PA_HIGH);
+  //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
+  radio.setDataRate(RF24_250KBPS);
+
+  //RECEIVE CODE (RECEIVE)
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+
   radio.startListening();
+  radio.printDetails();
 }
 
 /**** GENERAL ****/
